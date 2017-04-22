0fb48bad742c33d1a052e1c57b02f4753dd9a383
樋口大樹 <daikiit27@gmail.com>
2017-04-22 17:46:11 +0900

    復習します。

--- CheckTest-Day4/ViewController.swift
+++ CheckTest-Day4/ViewController.swift
@@ -9,16 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //名前を表示する
+    @IBOutlet var machineUILabel: UILabel!
+    
+    //名前を格納する配列
+    let name: [String] = ["iphone","ipad","apple watch"]
+    
+    //
+    var index: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        machineUILabel.text = name[index]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func back() {
+        
+    }
+    
+    @IBAction func next() {
+        
+    }
+    
+    @IBAction func first() {
+        
+    }
+    
+    @IBAction func second() {
+        
+    }
+    
+    @IBAction func third() {
+        
+    }
+    
+    
 
 
 }
