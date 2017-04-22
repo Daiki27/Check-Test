336ddb28fa19ad64f36f59b14d429386a7217a13
樋口大樹 <daikiit27@gmail.com>
2017-04-22 15:40:55 +0900

    Initial Commit

---
+++ CheckTest-Day4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckTest-Day4
+//
+//  Created by 樋口大樹 on 2017/04/22.
+//  Copyright © 2017年 樋口大樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
